8cbc4a3b3c05bad4f923a83784f192d4f336e5d7
Jiseop Han <jshan2017@gmail.com>
2019-01-21 16:27:25 +0900

    added: title query using regex

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ def get_document(title, lang):
     print('you are trying to get : '+ title)
     conn = sqlite3.connect("res/documents.db")
     cur= conn.cursor()
-    sql = "SELECT article FROM wiki WHERE title = ? ORDER BY time DESC LIMIT 1"
-    cur.execute(sql, (title,))
+    sql = "SELECT article FROM wiki WHERE title LIKE ? ORDER BY time DESC LIMIT 1"
+    cur.execute(sql, ('%'+title+'%',))
     rows = cur.fetchall()
     articleArray=None
     for row in rows:
@@ -32,14 +32,40 @@ def get_document(title, lang):
     print(result)
     conn.close()
     return jsonify({"result":result})
+@app.route('/documents/<string:title>/<string:lang>/raw', methods=['GET'])
+def get_raw_document(title):
+    print('get raw versions of : '+ title)
+    conn = sqlite3.connect("res/documents.db")
+    cur= conn.cursor()
+    sql = "SELECT article FROM wiki WHERE title LIKE ? ORDER BY time DESC LIMIT 1"
+    cur.execute(sql, ('%'+title+'%',))
+    rows = cur.fetchall()
+    result=rows[0][0]
+    print(result)
+    return jsonify({"result":result})
+
+@app.route('/documents/<string:title>', methods=['POST'])
+def post_document(title):
+    print('you are trying to post : '+ title)
+    article=str(request.get_json()['article'])
+    article=article.replace("'",'"')
+    print(article)
+    conn = sqlite3.connect("res/documents.db")
+    cur= conn.cursor()
+    sql = "INSERT INTO wiki VALUES (?,?, ?)"
+    now = int(time.time())
+    cur.execute(sql, (title, now, article))
+    conn.commit()
+    conn.close()
+    return 'success posting'
 
 @app.route('/documents/<string:title>/versions', methods=['GET'])
 def get_all_version(title):
     print('get every versions of : '+ title)
     conn = sqlite3.connect("res/documents.db")
     cur= conn.cursor()
-    sql = "SELECT time FROM wiki WHERE title = ?"
-    cur.execute(sql, (title,))
+    sql = "SELECT time FROM wiki WHERE title LIKE ?"
+    cur.execute(sql, ('%'+title+'%',))
     rows = cur.fetchall()
     versions=[]
     for row in rows:
@@ -51,8 +77,8 @@ def get_raw_version(title, version):
     print('get'+str(version)+ 'th versions of : '+ title)
     conn = sqlite3.connect("res/documents.db")
     cur= conn.cursor()
-    sql = "SELECT article FROM wiki WHERE title = ? AND time = ?"
-    cur.execute(sql, (title,version,))
+    sql = "SELECT article FROM wiki WHERE title LIKE ? AND time = ?"
+    cur.execute(sql, ('%'+title+'%',version,))
     rows = cur.fetchall()
     result=rows[0][0]
     print(result)
@@ -62,8 +88,8 @@ def get_rendered_version(title, version, lang):
     print('you are trying to get : '+ title)
     conn = sqlite3.connect("res/documents.db")
     cur= conn.cursor()
-    sql = "SELECT article FROM wiki WHERE title = ? AND time = ?"
-    cur.execute(sql, (title,version,))
+    sql = "SELECT article FROM wiki WHERE title LIKE ? AND time = ?"
+    cur.execute(sql, ('%'+title+'%',version,))
     rows = cur.fetchall()
     articleArray=None
     for row in rows:
@@ -79,20 +105,7 @@ def get_rendered_version(title, version, lang):
     print(result)
     conn.close()
     return jsonify({"result":result})
-@app.route('/documents/<string:title>', methods=['POST'])
-def post_document(title):
-    print('you are trying to post : '+ title)
-    article=str(request.get_json()['article'])
-    article=article.replace("'",'"')
-    print(article)
-    conn = sqlite3.connect("res/documents.db")
-    cur= conn.cursor()
-    sql = "INSERT INTO wiki VALUES (?,?, ?)"
-    now = int(time.time())
-    cur.execute(sql, (title, now, article))
-    conn.commit()
-    conn.close()
-    return 'success posting'
+
 
 def gTranslate(inText, lang):
     text =u'{}'.format(inText)
